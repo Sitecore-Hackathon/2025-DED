8de05a81ac99c76412948ee2b81e1a367556206d
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2025-03-07 22:25:53 -0500

    Adding initial scaffold and missing shadcn components for forms

--- src/app/settings/instance/page.tsx
+++ src/app/settings/instance/page.tsx
@@ -1,99 +1,94 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { ItemListingComponent } from "@/components/items/item-listing";
+import { AppSidebar } from '@/components/app-sidebar';
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
 import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { ITreeItem } from "@/models/ITreeItem";
-import { Separator } from "@radix-ui/react-separator";
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { ITreeItem } from '@/models/ITreeItem';
+import { Separator } from '@radix-ui/react-separator';
 
 const initialData: ITreeItem[] = [
-    {
-        id: "1",
-        name: "Home",
+  {
+    id: '1',
+    name: 'Home',
+    children: [
+      {
+        id: '1-1',
+        name: 'About Us',
+      },
+      {
+        id: '1-2',
+        name: 'Product Development',
         children: [
-            {
-                id: "1-1",
-                name: "About Us",
-            },
-            {
-                id: "1-2",
-                name: "Product Development",
-                children: [
-                    { id: "1-2-1", name: "Engineering" },
-                    {
-                        id: "1-2-2",
-                        name: "Design",
-                        children: [
-                            { id: "1-2-2-1", name: "UI Design" },
-                            { id: "1-2-2-2", name: "UX Research" },
-                        ],
-                    },
-                    { id: "1-2-3", name: "QA" },
-                ],
-            },
-            {
-                id: "1-3",
-                name: "Marketing",
-                children: [
-                    { id: "1-3-1", name: "Digital Marketing" },
-                    { id: "1-3-2", name: "Content Creation" },
-                ],
-            },
+          { id: '1-2-1', name: 'Engineering' },
+          {
+            id: '1-2-2',
+            name: 'Design',
+            children: [
+              { id: '1-2-2-1', name: 'UI Design' },
+              { id: '1-2-2-2', name: 'UX Research' },
+            ],
+          },
+          { id: '1-2-3', name: 'QA' },
         ],
-    },
-    {
-        id: "2",
-        name: "Project Roadmap",
+      },
+      {
+        id: '1-3',
+        name: 'Marketing',
         children: [
-            { id: "2-1", name: "Q1 Goals" },
-            { id: "2-2", name: "Q2 Goals" },
-            { id: "2-3", name: "Q3 Goals" },
-            { id: "2-4", name: "Q4 Goals" },
+          { id: '1-3-1', name: 'Digital Marketing' },
+          { id: '1-3-2', name: 'Content Creation' },
         ],
-    },
+      },
+    ],
+  },
+  {
+    id: '2',
+    name: 'Project Roadmap',
+    children: [
+      { id: '2-1', name: 'Q1 Goals' },
+      { id: '2-2', name: 'Q2 Goals' },
+      { id: '2-3', name: 'Q3 Goals' },
+      { id: '2-4', name: 'Q4 Goals' },
+    ],
+  },
 ];
 
 export default function InstanceSetupPage() {
-    return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-[orientation=vertical]:h-4"
-                        />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="/">
-                                        Dashboard
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block" />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>
-                                        Instance Configuration
-                                    </BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                    </div>
-                </header>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">WIP</div>
-            </SidebarInset>
-        </SidebarProvider>
-    );
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Instance Configuration</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">WIP</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
