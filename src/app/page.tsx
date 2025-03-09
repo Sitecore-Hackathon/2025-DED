843c74407b8e142a6d994a790f3310ab49cce6bd
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2025-03-08 19:25:11 -0500

    UI on dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="container mx-auto py-6">
+        <div className="container mx-auto py-6 px-4 md:px-6">
           <h2 className="text-lg font-semibold mb-4">Dashboard</h2>
           {instances ? (
             <>
